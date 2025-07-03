2d9da2080185b8bea72481a31a30b9f189a3b0f6
wormchenli <wormchenli@gmail.com>
2025-07-03 17:58:17 -0600

    added skip-list

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,11 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        I'm Li Chen
       </h1>
+      <p className="text-sm mb-8 mt-[-20px]">@ Calgary, AB, Canada</p>
       <p className="mb-4">
-        {`I'm Li Chen, a software engineer with a passion for building web applications.
+        {`A software engineer with a passion for building web applications.
         I specialize in the front-end stack, especially React, and have strong experience with TypeScript, Python, Java, and Spring Boot.
         I'm also passionate about data engineering and data science.`}
       </p>
